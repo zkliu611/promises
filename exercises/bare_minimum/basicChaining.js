ed698e3c8ebcb540c4da8e3567af95cd888bb004
zkliu611 <zkliu61@gmail.com>
2018-12-03 17:30:46 -0800

    work on basicChaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,12 +10,33 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+const { getGitHubProfileAsync } = require('./promisification');
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return getGitHubProfileAsync(user)
+    .then((body) => {
+      console.log(body);
+    })
+    .catch((err) => {
+      throw new Error('User already exists!')
+    });
 };
+
+return db.findUserInDatabaseAsync(user)
+    .then(function(existingUser) {
+      if (existingUser) {
+        throw new Error('User already exists!') // Head straight to `catch`. Do not pass Go, do not collect $200
+      } else {
+        return user; // Return a synchronous value
+      }
+    })
+    .then(function(newUser) {
+      return db.hashPasswordAsync(newUser) // Return a promise
+    })
+    .then(function(securedUser) {
+      return db.createAndSaveUserAsync(securedUser) // Return another promise
+    })
 
 // Export these functions so we can test them
 module.exports = {
